c4215c44b5d6eb4d18c912faff6db1f1be0b98db
thetechprepper <info@thetechprepper.com>
2025-08-13 07:02:54 -0700

    Post R5 Build 3 Fixes - Fixed et-aircraft test case

--- tests/test-et-aircraft.sh
+++ tests/test-et-aircraft.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Author   : Gaston Gonzalez
 # Date     : 9 July 2025
+# Updated  : 13 August 2025
 # Purpose  : Test et-aircraft installation
 
-OUT=$(which et-aircraft && which et-aircraft-app)
+OUT=$(which et-aircraft-app)
 exit $?
