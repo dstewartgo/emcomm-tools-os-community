81870f72750e36043b8c5089f3bb68f81dcdda35
thetechprepper <info@thetechprepper.com>
2025-09-04 06:07:41 -0700

    et-predict - Added et-predict to the installer. Note: the current application is just a skeleton. There is only a base map with logic yet

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 #
 # Author  : Gaston Gonzalez
 # Date    : 16 March 2024
-# Updated : 16 August 2025
+# Updated : 4 September 2025
 # Purpose : Main installer for EmComm Tools Community (ETC)
 
 . ./env.sh
@@ -87,3 +87,4 @@ exitIfNotRoot
 
 # Install prediction tools
 ./install-voacap.sh
+./install-et-predict.sh
---
+++ tests/test-et-predict.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Author   : Gaston Gonzalez
+# Date     : 4 September 2025
+# Purpose  : Test et-predict installation
+
+OUT=$(which et-predict-app)
+exit $?
